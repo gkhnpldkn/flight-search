260b993f36426c29309cb95680f7748cdc580bca
gkhnpldkn <gkhnpldkn@gmail.com>
2023-12-23 01:14:04 +0300

    kullanılmayan değişkenler kaldırıldı

--- src/main/java/org/gokhan/dto/request/SignUpRequest.java
+++ src/main/java/org/gokhan/dto/request/SignUpRequest.java
@@ -13,8 +13,6 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpRequest {
-    private String firstName;
-    private String lastName;
     private String email;
     private String password;
     private List<String> roles;
